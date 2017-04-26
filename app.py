3a4248b47fcae5b60a7314fe1bf3197400a9e145
Aditya Duri <aditya.duri@gmail.com>
2017-04-26 00:20:15 -0500

    Updating reviews with tags

--- app.py
+++ app.py
@@ -295,9 +295,9 @@ def room(building, classname):
         avg_rating = 0
     else:
         avg_rating = 0.5 + (float(sum(sentiments)/num_words)/1.0)
-        if avg_rating < 0.3:
+        if avg_rating < 0.4:
             avg_rating = 0
-        if avg_rating > 0.7:
+        if avg_rating > 0.6:
             avg_rating = 1
     color = Color(hue=avg_rating*0.4, saturation=0.9, luminance=0.9).hex
     words = [[word['word'], word['freq']] for word in words.values()]
